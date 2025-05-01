fc6879ceba50b68d6a538e281783cefeb2f151ba
Rohan0305 <rohanvallapalli@gmail.com>
2025-04-30 20:39:29 -0400

    Get Messages

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,10 +1,13 @@
-import { DUMMY_MESSAGES } from "../../dummy_data/dummy";
+import { useGetMessages } from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
+	const {loading, messages} = useGetMessages();
 	return (
 		<div className='px-4 flex-1 overflow-auto'>
-			{DUMMY_MESSAGES.map((message) => (
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{messages?.map((message) => (
 				<Message key={message.id} message={message} />
 			))}
 		</div>
---
+++ frontend/src/hooks/useGetMessages.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import useConversation from "../zustand/useConversation";
+
+export const useGetMessages = () => {
+  const [loading, setLoading] = useState(false);
+  const { messages, selectedConversation, setMessages } = useConversation();
+
+  useEffect(() => {
+    const getMessages = async () => {
+      if (!selectedConversation) return;
+      setLoading(true);
+      setMessages([]);
+      try {
+        const res = await fetch(`/api/messages/${selectedConversation?.id}`);
+        const data = await res.json();
+        if (!res.ok) {
+            throw new Error(data.error || "An error occurred");
+        }
+        setMessages(data);
+      } catch (error: any) {
+        toast.error(error.message || "Failed to load messages");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getMessages();
+  }, [selectedConversation, setMessages]);
+  return {loading, messages}
+};
