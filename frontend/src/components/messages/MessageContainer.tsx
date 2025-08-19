ed9950115095559b003246b044af4bb5b62f0d91
Rohan0305 <rohanvallapalli@gmail.com>
2025-08-19 10:28:01 -0400

    Incorrect string fix

--- frontend/src/components/messages/MessageContainer.tsx
+++ frontend/src/components/messages/MessageContainer.tsx
@@ -33,7 +33,7 @@ const {authUser} = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome {authUser?.fullName} (${authUser?.username})</p>
+        <p>Welcome {authUser?.fullName} ({authUser?.username})</p>
         <p>Select a chat to start messaging</p>
         <MessageCircle className="text-3xl md:text-6xl text-center" />
       </div>
